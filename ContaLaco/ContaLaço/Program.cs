cf9c5041d81309ee0519944abb7001e55b3221e9
FilipeAugustoVO <43390392+FilipeAugustoVO@users.noreply.github.com>
2018-12-12 18:54:46 -0300

    Add files via upload

---
+++ ContaLaco/ContaLaço/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ContaLaço
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 2;
+            int j;
+            for (i = 2; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
