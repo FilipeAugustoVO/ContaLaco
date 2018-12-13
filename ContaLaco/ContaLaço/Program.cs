6a74076cd5119d9dc54370b3c20a59e43fd63207
FilipeAugustoVO <43390392+FilipeAugustoVO@users.noreply.github.com>
2018-12-13 19:10:25 -0300

    Add files via upload

--- ContaLaco/ContaLaço/Program.cs
+++ ContaLaco/ContaLaço/Program.cs
@@ -6,11 +6,12 @@ namespace ContaLaço
     {
         static void Main(string[] args)
         {
-            int i = 2;
-            int j;
-            for (i = 2; i < 10; i++)
+            for (int i = 2; i <= 102; i++)
             {
-                Console.WriteLine(i);
+                int val = i % 2 == 1 ? i * -1 : i;
+                Console.WriteLine(val);
+                // integral val igual i divido por 2, resto é igual 1, condição se i multiplocado por - 1 ou condição é igual i; 
+                // pergunta pro programa se o resto da conta é igual um, e ai se i é i multiplocado por -1 ou se i é i
             }
         }
     }
